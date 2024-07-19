44852a33e034d3e753d1a3ef1c4e82a2a3554dcd
Escouflenfer <rigautwilliam@gmail.com>
2024-07-19 16:20:24 +0200

    added mkdir commands for ./data and ./results

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ python -m venv .venv
 source .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+mkdir ./data
+mkdir ./data/EDX ./data/MOKE ./data/XRD
+mkdir ./results
+mkdir ./results/EDX ./results/MOKE ./results/XRD
